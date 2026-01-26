c2af035a43fcf911ff743207fa4133af5218e0fa
Guillermo ayuda <guilleayuda04@gmail.com>
2026-01-26 10:28:51 +0100

    Final de clase 26/01/2026

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -12,10 +12,26 @@
         <form action="{{ route('home') }}" method="GET">
             <button type="submit">Ir a Home</button>
         </form>
-        <form action="{{ route('sobre') }}" method="GET">
-            <button type="submit">Sobre Nosotros</button>
+        <form action="{{ route('socios') }}" method="GET">
+            <button type="submit">Hazte socio</button>
+        </form>
+        <form action="{{ route('noticias') }}" method="GET">
+            <button type="submit">Noticias</button>
+        </form>
+        <form action="{{ route('contacta') }}" method="GET">
+            <button type="submit">Contacto</button>
         </form>
     </nav>
 </header>
+<main>
+    <section>Foto De portada</section>
+    <section>
+        <article>Noticia de bienvenida 1</article>
+        <article>Noticia de bienvenida 2</article>
+    </section>
+</main>
+<footer>
+    <h3>© 2026 Albalate FS</h3>
+</footer>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,21 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('inicio');
+    return view('welcome', ['usuarios' => ['juan', 'manuel']]);
 });
 Route::get('/home', function () {
     return view('home');
 })->name('home');
 
-Route::get('/sobre', function (){
-    return view('sobre');
-})->name('sobre');
-Route::view('/contacta', 'contacta');
-Route::view('/noticias', 'noticias');
+Route::get('/socios', function (){
+    return view('socios');
+})->name('socios');
+
+Route::get('/contacta', function(){
+    return view ('contacta');
+})-> name ('contacta');
+
+Route::get('/noticias', function(){
+    return view ('noticias');
+})-> name ('noticias');
 
